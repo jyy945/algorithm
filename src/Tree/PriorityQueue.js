8f8ffeaf9d2cb89b56400369f4e62a347d99bae4
jyying <1101603804@qq.com>
2021-04-07 23:13:32 +0800

    1

---
+++ src/Tree/PriorityQueue.js
@@ -0,0 +1,68 @@
+function PriorityQueue(){
+    this.arr = [];
+}
+PriorityQueue.prototype.enQueue = function(data){
+    this.arr.push(data);
+    this.upAdjust();
+};
+PriorityQueue.prototype.deQueue = function(){
+    const data = this.arr[0];
+    const last = this.arr.pop();
+    if(last === undefined) return;
+    if(this.arr.length === 0) return last;
+    this.arr[0] = last;
+    this.downAdjust();
+    return data;
+};
+PriorityQueue.prototype.upAdjust = function(){
+    let length = this.arr.length;
+    let childIndex = length - 1;
+    let parentIndex = Math.floor((childIndex - 1) / 2);
+    let temp = this.arr[childIndex];
+    while(childIndex > 0 && temp < this.arr[parentIndex]){
+        this.arr[childIndex] = this.arr[parentIndex];
+        childIndex = parentIndex;
+        parentIndex = Math.floor((parentIndex - 1) / 2);
+    }
+    this.arr[childIndex] = temp;
+};
+PriorityQueue.prototype.downAdjust = function(){
+    let length = this.arr.length;
+    let parentIndex = 0;
+    let childIndex = 1;
+    let temp = this.arr[parentIndex];
+    while(childIndex < length){
+        if(childIndex + 1 < length && this.arr[childIndex] > this.arr[childIndex + 1]){
+            childIndex++;
+        }
+        if(temp <= this.arr[childIndex]) break;
+        this.arr[parentIndex] = this.arr[childIndex];
+        parentIndex = childIndex;
+        childIndex = childIndex * 2 + 1;
+    }
+    this.arr[parentIndex] = temp;
+};
+
+const queue = new PriorityQueue();
+queue.enQueue(3);
+queue.enQueue(5);
+queue.enQueue(10);
+queue.enQueue(2);
+queue.enQueue(7);
+console.log(queue.arr);
+console.log(queue.deQueue());
+console.log(queue.arr);
+console.log(queue.deQueue());
+console.log(queue.arr);
+console.log(queue.deQueue());
+console.log(queue.arr);
+console.log(queue.deQueue());
+console.log(queue.arr);
+console.log(queue.deQueue());
+console.log(queue.arr);
+console.log(queue.deQueue());
+console.log(queue.arr);
+
+
+
+
